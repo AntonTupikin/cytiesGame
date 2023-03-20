3ea4ae75e63698c2d5c2799d77a86df3c8a2e1e2
AntonTupikin <antontupikin@icloud.com>
2023-03-20 15:15:02 +0500

    2.3

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,38 +1,37 @@
 package org.example;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Client {
 
-    public static void start() throws InterruptedException {
-        Thread.sleep(3000);
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
         try (Socket clientSocket = new Socket("localhost", 8585)) // этой строкой мы запрашиваем у сервера доступ на соединение)
         {
-            System.out.println("Клиент создан");
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // поток чтения из сокета
                  BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); // поток записи в сокет
             ) {
-
-                System.out.println("Вы что-то хотели сказать? Введите это здесь:");
-
-                String word = reader.readLine();
-
-                out.write(word + "\n");
-                out.flush();
-                String serverWord = in.readLine();
-                System.out.println(serverWord);
+                String word = in.readLine();
+                System.out.println(word);
+                if (word.equals("????")) {
+                    String word1 = reader.readLine();
+                    out.write(word1 + "\n");
+                    out.flush();
+                } else {
+                    String word2 = reader.readLine();
+                    out.write(word2 + "\n");
+                    out.flush();
+                    String word3 = in.readLine();
+                    System.out.println(word3);
+                }
 
             } catch (IOException e) {
                 System.err.println(e);
 
             }
 
-            System.out.println("Клиент был закрыт...");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -8,6 +8,14 @@ public class Game {
         this.word=word;
     }
 
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
     public String check(String userWord){
         char c = word.charAt(word.length()-1);
         char b = userWord.charAt(0);
